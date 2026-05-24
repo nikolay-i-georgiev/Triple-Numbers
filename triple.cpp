a4c30f5e6a41626986b1333521ef8c196efe5207
Nikolay Georgiev <niky.i.georgiev@gmail.com>
2026-05-24 15:07:59 +0300

    Added multiplication and division

--- triple.cpp
+++ triple.cpp
@@ -7,3 +7,12 @@ Triple Triple::operator+(const Triple& o) const{
 Triple Triple::operator*(const Triple&o) const{
     return Triple(a*o.a, a*o.b+b*o.a, a*o.c+b*o.b+c*o.a);
 }
+
+Triple Triple::operator-(const Triple& o) const{
+    return Triple(a-o.a, b-o.b, c-o.c);
+}
+
+Triple Triple::operator/(const Triple& o) const{
+    return Triple(a/o.a, b/o.a-a*o.b/(o.a*o.a), c/o.a-b*o.b/(o.a*o.a)-a*o.c/(o.a*o.a)+a*o.b*o.b/(o.a*o.a*o.a));
+}
+
--- triple.hpp
+++ triple.hpp
@@ -2,7 +2,10 @@ struct Triple{
 double a,b,c;
 
 Triple(double a, double b, double c) : a(a), b(b), c(c) {}
+Triple(double a) : a(a), b(0), c(0) {}
 
 Triple operator+(const Triple& o) const;
+Triple operator-(const Triple& o) const;
 Triple operator*(const Triple& o) const;
+Triple operator/(const Triple& o) const;
 };
